37dc6b6fbc29f135adce969cc4defd859ec35857
Sagist <samvel.grigoryan.psy@gmail.com>
2024-02-20 11:16:32 +0400

    update redux

--- client/src/pages/auth/register.tsx
+++ client/src/pages/auth/register.tsx
@@ -6,6 +6,8 @@ import {wrapper} from "../../store";
 import MainLayout from "../../layouts/MainLayout";
 import TextField from "@mui/material/TextField";
 import {useInput} from "../../hooks/useInput";
+import axios from "axios";
+import Button from "@mui/material/Button";
 
 const Register = () => {
     const firstName = useInput('')
@@ -13,38 +15,53 @@ const Register = () => {
     const email = useInput('')
     const password = useInput('')
     const repassword = useInput('')
+    const register = async () => {
+        try {
+            const response = await axios.post('http://localhost:5000/auth/register', {
+                firstName: firstName.value,
+                lastName: lastName.value,
+                email: email.value,
+                password: password.value
+            })
+            console.log(response)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     return (
         <MainLayout title={"Track list for Music Platform"}>
             <Grid container justifyContent='center'>
                 <Card style={{width: 900}}>
-                    <Box p={3}>
+                    <Box p={3} component="form">
                         <h1>Register</h1>
-                            <TextField
-                                {...firstName}
-                                style={{marginTop: 10, width:'100%'}}
-                                label={"Enter your firstName"}
-                            />
-                            <TextField
-                                {...lastName}
-                                style={{marginTop: 10, width:'100%'}}
-                                label={"Enter your lastName"}
-                            />
-                            <TextField
-                                {...email}
-                                style={{marginTop: 10, width:'100%'}}
-                                label={"Enter your email"}
-                            />
-                            <TextField
-                                {...password}
-                                style={{marginTop: 10, width:'100%'}}
-                                label={"Enter your password"}
-                            />
-                            <TextField
-                                {...repassword}
-                                style={{marginTop: 10, width:'100%'}}
-                                label={"Enter your repassword"}
-                            />
+                        <TextField
+                            {...firstName}
+                            style={{marginTop: 10, width: '100%'}}
+                            label={"Enter your firstName"}
+                        />
+                        <TextField
+                            {...lastName}
+                            style={{marginTop: 10, width: '100%'}}
+                            label={"Enter your lastName"}
+                        />
+                        <TextField
+                            {...email}
+                            style={{marginTop: 10, width: '100%'}}
+                            label={"Enter your email"}
+                        />
+                        <TextField
+                            {...password}
+                            style={{marginTop: 10, width: '100%'}}
+                            label={"Enter your password"}
+                        />
+                        <TextField
+                            {...repassword}
+                            style={{marginTop: 10, width: '100%'}}
+                            label={"Enter your repassword"}
+                        />
+                        <Button onClick={register}>Register</Button>
+
                     </Box>
                 </Card>
             </Grid>
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,15 +1,11 @@
-
-
-// create a makeStore function
 import {Context, createWrapper, MakeStore} from "next-redux-wrapper";
 import {AnyAction, applyMiddleware, createStore} from "redux";
 import {reducer, RootState} from "./reducers";
-import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {thunk, ThunkDispatch} from "redux-thunk";
 
 const makeStore: MakeStore<RootState>
     = (context: Context) => createStore(reducer, applyMiddleware(thunk));
 
-// export an assembled wrapper
 export const wrapper = createWrapper<RootState>(makeStore, {debug: true});
 
 export type NextThunkDispatch = ThunkDispatch<RootState, void, AnyAction>
